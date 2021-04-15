4d22fd3206959db493a0b8a1beb3bab09441c838
trevorrich <72053995+trevorrich@users.noreply.github.com>
2021-04-15 15:19:29 -0400

    basic homescreen

--- main.cpp
+++ main.cpp
@@ -26,10 +26,42 @@ DigitalIn rightPB(p19);
 DigitalIn centerPB(p20);
 
 
+void homeScreen(){
+    uLCD.cls();
+    uLCD.color(WHITE);
+    //ALARM
+    uLCD.text_width(1.25); //4X size text
+    uLCD.text_height(1.25);
+    uLCD.locate(0,0);
+    uLCD.printf("Alarm: ");
+    //UPDATE WITH VARS
+    uLCD.printf("00:00");
+    uLCD.printf("am");
+    
+    //TIME
+    uLCD.text_width(2.5); //4X size text
+    uLCD.text_height(2.5);
+    uLCD.locate(1.5,2.5);
+    //UPDATE WITH VARS
+    uLCD.printf("12:00");
+    uLCD.printf("am");
+    
+    //MENU/MODE
+    uLCD.text_width(1.25); //4X size text
+    uLCD.text_height(1.25);
+    uLCD.locate(2,14);
+    uLCD.printf("Menu");
+    uLCD.locate(9,14);
+    uLCD.printf("Mode: ");
+    //GET MODE
+    uLCD.printf("mode");
+}
+
 int main() {
     uLCD.cls();
     uLCD.baudrate(BAUD_3000000); //jack up baud rate to max for fast display
     wait(1.0);
+    homeScreen();
     while(1) {
 
     }
